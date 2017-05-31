e84369c2cb3dc9807f39d611f4f16f08c532dff7
ksmithut <ksmith.ut@gmail.com>
2017-05-30 19:55:38 -0600

    correctly set timeout

--- test/express.test.js
+++ test/express.test.js
@@ -47,7 +47,7 @@ describe('mongoose-model', () => {
     return mockgoose.prepareStorage().then(() => {
       return mongoose.connect('mongodb://localhost/test')
     })
-  }, 0)
+  }, 120000)
 
   afterEach(() => mockgoose.helper.reset())
 
