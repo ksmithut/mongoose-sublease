de23316c73bed7ed9b2d98229355ce6f27c90701
ksmithut <github@ksmithut.com>
2020-06-10 20:46:36 -0600

    change timeout

--- test/express.test.js
+++ test/express.test.js
@@ -47,7 +47,7 @@ describe('mongoose-model', () => {
     return mockgoose.prepareStorage().then(() => {
       return mongoose.connect('mongodb://localhost/test')
     })
-  }, 240000)
+  }, 600000)
 
   afterEach(() => mockgoose.helper.reset())
 
